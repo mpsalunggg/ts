8ee5694396a6e4caf490455b24883a60cd1aa2ef
mpsalunggg <putrasatria893@gmail.com>
2025-03-02 14:59:12 +0700

    generic keyof

--- src/functions/generic.ts
+++ src/functions/generic.ts
@@ -34,3 +34,35 @@ logLength("Any String");
 // logLength(2); error
 
 logLength({ name: "John", length: 12 });
+
+// Generic keyof operator
+type Events = {
+  id: number;
+  date: Date;
+  type: "indoor" | "test";
+};
+
+type TypeKeyOf = keyof Events;
+
+const dateOfEvent: TypeKeyOf = "date";
+
+type Numeric = {
+  [key: number]: string;
+};
+
+type NumericKeyOf = keyof Numeric;
+
+type NumberAndString = {
+  [key: string]: string;
+};
+
+type NumberAndStringKeyOf = keyof NumberAndString;
+
+let numberObj: Numeric = {
+  1: "asd",
+};
+
+let numAndStringObj: NumberAndString = {
+  1: "test",
+  test: "Test",
+};
