4c680771ffad07e7a71d6fbc959dc9d2d52cf73e
mpsalunggg <putrasatria893@gmail.com>
2025-04-11 01:19:27 +0800

    example 2 abstract

--- src/abstract/abstract.ts
+++ src/abstract/abstract.ts
@@ -17,3 +17,22 @@ dog.makeSound(); // Woof!
 dog.move(); // Moving...
 
 // const a = new Animal(); ❌ ERROR: Cannot create an instance of an abstract class.
+
+// example 2
+type Holidays = {
+  date: Date;
+  reason: string;
+}[];
+
+abstract class Department {
+  protected abstract holidays: Holidays;
+  protected constructor(protected name: string) {}
+}
+
+class ItDepartment extends Department {
+  protected holidays: Holidays = [];
+}
+
+class AdminDepartment extends Department {
+  protected holidays: Holidays = [];
+}
