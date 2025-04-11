93522cf3f1d48924fd1c708d92ca40d6af0486e9
mpsalunggg <putrasatria893@gmail.com>
2025-04-11 23:57:26 +0800

    adding holidays

--- src/abstract/abstract.ts
+++ src/abstract/abstract.ts
@@ -27,12 +27,50 @@ type Holidays = {
 abstract class Department {
   protected abstract holidays: Holidays;
   protected constructor(protected name: string) {}
+
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
 }
 
 class ItDepartment extends Department {
   protected holidays: Holidays = [];
+
+  constructor() {
+    super("IT DEPARTMENT");
+  }
 }
 
 class AdminDepartment extends Department {
   protected holidays: Holidays = [];
+
+  constructor() {
+    super("ADMIN DEPARTMENT");
+  }
 }
+
+const itDepartment: ItDepartment = new ItDepartment();
+const adminDepartment: AdminDepartment = new AdminDepartment();
+
+const itHolidays: Holidays = [
+  {
+    date: new Date(2022, 11, 1),
+    reason: "IT Day",
+  },
+];
+
+const adminHolidays: Holidays = [
+  {
+    date: new Date(2025, 4, 1),
+    reason: "Idul Fitri",
+  },
+];
+
+itDepartment.addHolidays(itHolidays)
+adminDepartment.addHolidays(adminHolidays)
+
+console.log(itDepartment)
