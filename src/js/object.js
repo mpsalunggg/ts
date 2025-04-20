e7b48a899bf36fa46613aeb428ac27fdd21b9247
mpsalunggg <putrasatria893@gmail.com>
2025-04-20 20:25:39 +0700

    prototype in function

--- src/js/object.js
+++ src/js/object.js
@@ -16,3 +16,56 @@ const data = {
 };
 
 data.printAuthor();
+
+/*
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.points = 0;
+  }
+
+  login() {
+    console.log(this.name, 'Has logged in');
+  }
+
+  logout() {
+    console.log(this.name, 'Has logged out');
+  }
+
+  addPoint() {
+    this.points++;
+    console.log('total points', this.points);
+  }
+}
+
+const user = new User('John', 'john@email.com');
+
+console.log(user);
+*/
+
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+  this.points = 0;
+}
+
+User.prototype.login = function () {
+  console.log(this.name, 'Has logged in');
+};
+
+User.prototype.logout = function () {
+  console.log(this.name, 'Has logged out');
+};
+
+User.prototype.addPoint = function () {
+  this.points++;
+  console.log('total points', this.points);
+};
+
+const user = new User('John', 'john@email.com');
+
+user.login()
+user.addPoint()
+user.logout()
+user.addPoint()
