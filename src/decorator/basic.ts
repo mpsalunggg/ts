b3d34683998b4a8a6daac648d9ffc5bb2c7cc3e6
mpsalunggg <putrasatria893@gmail.com>
2025-04-24 21:21:30 +0700

    decorator basic 2

--- src/decorator/basic.ts
+++ src/decorator/basic.ts
@@ -1,13 +1,14 @@
+import { json } from "stream/consumers";
+
 const methodLogger = (originalMethod: any, context: any) => {
   console.log(originalMethod);
-  console.log(context)
+  console.log(context);
 };
 
 const classDecorator = (target: any, context: any) => {
-  console.log(String(target))
-  console.log(context)
-}
-
+  console.log(String(target));
+  console.log(context);
+};
 
 @classDecorator
 class DecoratorExample {
@@ -15,4 +16,29 @@ class DecoratorExample {
   greet() {
     console.log("hellooo broo");
   }
+}
+
+console.log("======");
+const Logger = (constructor: Function) => {
+  console.log(String(constructor.name));
+};
+
+@Logger
+class Yuhu {
+  name = "putra";
+}
+
+console.log("=======");
+function LogProperty(value: undefined, context: ClassFieldDecoratorContext) {
+  console.log(
+    `Property "${String(context.name)}" didesain untuk:`,
+    context.kind,
+  );
+  console.log(value),
+  console.log(context)
+}
+
+class Product {
+  @LogProperty
+  price: number = 100;
 }
