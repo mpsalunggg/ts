442d3a3500db5116d19e881996cefc4ef4914b00
mpsalunggg <putrasatria893@gmail.com>
2025-07-03 09:35:53 +0700

    decorator stage 2 prototype

--- src/decorator-stage-2/basic.ts
+++ src/decorator-stage-2/basic.ts
@@ -1,15 +1,38 @@
-function FirstDecorator(title: string) {
-  return (constructor: Function) => {
-    console.log(title + ": Decorator Invoked");
-    console.log(constructor);
+enum Manufacturers {
+  boeing = "boeing",
+  airbus = "airbus",
+}
+
+interface AircraftInterface {
+  _aircraftModel: string;
+  prototype?: any;
+  origin?: string;
+  manufacturer?: string;
+  type?: string;
+}
+
+function AircraftManufacturer(manufacturer: Manufacturers) {
+  return (target: Function) => {
+    if (manufacturer === Manufacturers.airbus) {
+      target.prototype.origin = "United States Of America";
+      target.prototype.manufacturer = Manufacturers.airbus;
+      target.prototype.type = "Jet";
+    } else {
+      target.prototype.origin = "France";
+      target.prototype.manufacturer = Manufacturers.boeing;
+      target.prototype.type = "Helicopter";
+    }
   };
 }
-@FirstDecorator("Testttt")
-class Aircraft {
+
+@AircraftManufacturer(Manufacturers.airbus)
+class Airplane implements AircraftInterface {
   constructor(
     public _aircraftModel: string,
     private pilot: string,
-  ) {}
+  ) {
+    console.log("Aircraft Class Instantiated");
+  }
 
   public pilotName() {
     console.log(this.pilot);
@@ -19,3 +42,7 @@ class Aircraft {
     return this._aircraftModel;
   }
 }
+
+const airplane: AircraftInterface = new Airplane("Airbus A380", "John");
+
+console.log(airplane);
