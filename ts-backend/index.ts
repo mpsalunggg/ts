00e2e553f31aa2f11040796cf8ed2f253842458d
mpsalunggg <putrasatria893@gmail.com>
2025-08-21 22:14:40 +0700

    add routes config to index

--- ts-backend/index.ts
+++ ts-backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express'
+import { addRoutes } from './src/config/routes.config'
 
 const app: Express = express()
 const port = 3001
@@ -6,6 +7,8 @@ const port = 3001
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server testing')
 })
+
+addRoutes(app)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
