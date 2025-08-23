a18c070be307fe0b4262b382ce9e759ee9713cf8
mpsalunggg <putrasatria893@gmail.com>
2025-08-23 21:01:15 +0700

    setup database

--- ts-backend/index.ts
+++ ts-backend/index.ts
@@ -1,8 +1,12 @@
 import express, { Express, Request, Response } from 'express'
 import { addRoutes } from './src/config/routes.config'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app: Express = express()
-const port = 3001
+const port = process.env.PORT
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server testing')
@@ -10,6 +14,22 @@ app.get('/', (_req: Request, res: Response) => {
 
 addRoutes(app)
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+async function bootstrap() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Cannot read env database url!')
+  }
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      dbName: process.env.DATABASE_NAME,
+    })
+    console.log('Connnected To MongoDB')
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`)
+    })
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+bootstrap()
