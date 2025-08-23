829e8656609aa33b3021104930bedaac77a5d07f
mpsalunggg <putrasatria893@gmail.com>
2025-08-23 22:56:04 +0700

    create, get all tasks, and update tasks

--- ts-backend/src/tasks/tasks.controller.ts
+++ ts-backend/src/tasks/tasks.controller.ts
@@ -1,30 +1,44 @@
+import { Request, Response } from 'express'
 import { injectable, inject } from 'inversify'
 import { UserController } from '../user/user.controller'
+import { IPartialTaskWithId, ITask } from './task.interface'
+import { Task } from './task.schema'
+import { Document } from 'mongoose'
 
 export class TasksController {
   constructor(@inject(UserController) private userController: UserController) {}
 
-  public handleGetTasks() {
-    return [
-      {
-        title: 'This is a title',
-        description: 'Task description',
-      },
-    ]
+  public async handleGetTasks(req: Request, res: Response) {
+    const tasks: ITask[] = await Task.find()
+    return tasks
   }
 
-  public handlePostTasks() {
-    console.log(this.userController.getUser())
-    return {
-      title: 'This is a title post',
-      description: 'Task description',
-    }
+  public async handlePostTasks(req: Request<{}, {}, ITask>, res: Response) {
+    const task: Document<unknown, any, ITask> = new Task(req.body)
+
+    await task.save()
+
+    return task
   }
 
-  public handlePatchTasks() {
-    return {
-      title: 'This is a title patch',
-      description: 'Task description',
+  public async handlePatchTasks(
+    req: Request<{}, {}, IPartialTaskWithId>,
+    res: Response
+  ) {
+    const task = await Task.findById(req.body['_id'])
+
+    if (task) {
+      task.title = req.body.title ? req.body.title : task.title
+      task.description = req.body.description
+        ? req.body.description
+        : task.description
+      task.dueDate = req.body.dueDate ? req.body.dueDate : task.dueDate
+      task.priority = req.body.priority ? req.body.priority : task.priority
+      task.status = req.body.status ? req.body.status : task.status
+
+      await task.save()
     }
+
+    return task
   }
 }
--- ts-backend/src/tasks/tasks.router.ts
+++ ts-backend/src/tasks/tasks.router.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express'
 import { TasksController } from './tasks.controller'
 import { injectable, inject } from 'inversify'
+import { IPartialTaskWithId, ITask } from './task.interface'
 
 @injectable()
 export class TasksRouter {
@@ -14,19 +15,28 @@ export class TasksRouter {
   }
 
   private initializeRoutes() {
-    this.router.get('/', (req: Request, res: Response) => {
-      const newTask = this.tasksController.handleGetTasks()
-      res.send(newTask).json()
+    this.router.get('/', async (req: Request, res: Response) => {
+      const allTasks = await this.tasksController.handleGetTasks(req, res)
+      res.json(allTasks)
     })
 
-    this.router.post('/create', (req: Request, res: Response) => {
-      const newTask = this.tasksController.handlePostTasks()
-      res.send(newTask).json()
-    })
+    this.router.post(
+      '/create',
+      async (req: Request<{}, {}, ITask>, res: Response) => {
+        const newTask = await this.tasksController.handlePostTasks(req, res)
+        res.json(newTask)
+      }
+    )
 
-    this.router.patch('/update', (req: Request, res: Response) => {
-      const newTask = this.tasksController.handlePatchTasks()
-      res.send(newTask).json()
-    })
+    this.router.patch(
+      '/update',
+      async (req: Request<{}, {}, IPartialTaskWithId>, res: Response) => {
+        const updatedTask = await this.tasksController.handlePatchTasks(
+          req,
+          res
+        )
+        res.json(updatedTask)
+      }
+    )
   }
 }
